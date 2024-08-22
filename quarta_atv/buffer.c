cde01bba2d6f4a142a7aaa1f5e540912c68e0c45
VyctorCosta <vyctorcosta00@gmail.com>
2024-08-22 08:06:38 -0300

    save

---
+++ quarta_atv/buffer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  char vetor1[] = "Nome";
+  char vetor2[5];
+
+  scanf("%s", vetor2);
+
+  printf("vetor 1 = %s\n", vetor1);
+  printf("vetor 2 = %s\n", vetor2);
+}
